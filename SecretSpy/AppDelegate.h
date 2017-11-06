eb4ab233baae39cd5f3768ef657fb846f7678d4a
azatmelikyan <mr.azatmelikyan@gmail.com>
2017-11-06 23:39:35 +0400

    initial commit

---
+++ SecretSpy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecretSpy
+//
+//  Created by User on 11/6/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecretSpy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecretSpy
+//
+//  Created by User on 11/6/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
